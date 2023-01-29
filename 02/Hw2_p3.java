8e1710df404c5941efdeba26e94db2d392653c30
brookelove <brookelove@Brookes-iMac-2.local>
2023-01-28 18:15:53 -0800

    removing and adding elments based on recursion

--- 02/Hw2_p3.java
+++ 02/Hw2_p3.java
@@ -4,9 +4,31 @@ public class Hw2_p3 {
 	// implement reverse method
 	// you may want to write a separate method with additional parameters, which is
 	// recursive
+	public static void helper(DoublyLinkedList<Integer> intList, int i) {
+		// System.out.println(intList);
+		// System.out.println(i);
+		// 1.base case
+		if (i <= 0) {
+			return;
+		}
+		// 2. removed the first element in the list and stire its value
+
+		int currentFirst = intList.removeFirst();
+		System.out.println("Placed on Stack: " + currentFirst);
+		// 3. recursive call
+		helper(intList, i - 1);
+		// 4. add the most recenetly removed element to the end of the list
+		// intList.addFirst(currentFirst);
+		System.out.println("Removed from stack: " + currentFirst);
+	}
 
 	public static void reverse(DoublyLinkedList<Integer> intList) {
 		// complete this method
+		intList.last();
+		int index = intList.size();
+		// System.out.println(intList.last()); // gets the last item in the array
+		// System.out.println(intList.size()); //gets the size of the list
+		helper(intList, index);
 	}
 
 	// use the main method for testing
