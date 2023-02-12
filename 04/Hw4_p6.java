6045286ce673ded94887292174da5d5ec811de1e
brookelove <brookelove@Brookes-iMac-2.local>
2023-02-11 20:40:50 -0800

    adding comments to code

---
+++ 04/Hw4_p6.java
@@ -0,0 +1,39 @@
+/*
+ * create a HashMap instance myMap
+ * create an ArrayList instance myArrayList create a LinkedList instanc
+ * myLinkedList
+ * 
+ * Repeat the following 10 times and calculate average total insertion time and
+ * average total search time for each data structure
+ * 
+ * generate 100,000 distinct random integers in the range [1, 1,000,000] and
+ * store them in the array of integers insertKeys[ ]
+ * 
+ * // begin with empty myHap, myArrayList, and myLinkedList each time
+ * 
+ * // Insert keys one at a time but measure only the total time (not individual
+ * insert // time)
+ * // Use put method for HashMap, e.g., myMap.put(insertKeys[i], i)
+ * // Use add method for ArrayList and LinkedList
+ * 
+ * insert all keys in insertKeys [ ] into myMap and measure the total insert
+ * time
+ * insert all keys in insertKeys [ ] into myArrayList and measure the total
+ * insert time
+ * insert all keys in insertKeys [ ] into myLinkedList and measure the total
+ * insert time
+ * 
+ * generate 100,000 distinct random integers in the range [1, 2,000,000] and
+ * store them in the array searchKeys[ ].
+ * 
+ * // Search keys one at a time but measure only total time (not individual
+ * search // time)
+ * // Use containsKey method for HashMap
+ * // Use contains method for ArrayList and Linked List
+ * 
+ * search myMap for all keys in searchKeys[ ] and measure the total search time
+ * search myArrayList for all keys in searchKeys[ ] and measure the total search
+ * time search myLinkedList for all keys in searchKeys[ ] and measure the total
+ * search time
+ * 
+ */
