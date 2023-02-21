289d3f932cf5a769841668e7297b7797fdac2064
Brooke Love <savienloveb@gmail.com>
2023-02-20 19:31:29 -0500

    maintaining clean code

--- 05/Hw5_P7.java
+++ 05/Hw5_P7.java
@@ -1,13 +1,14 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Random;
 
 class Hw7 {
     // code for insertion sort
     // Citation:
-    public static void insertionSort(char[] data) {
+    public static void insertionSort(int[] data) {
         int n = data.length;
         for (int k = 1; k < n; k++) {
-            char cur = data[k];
+            int cur = data[k];
             int j = k;
             while (j > 0 && data[j - 1] > cur) {
                 data[j] = data[j - 1];
@@ -46,6 +47,7 @@ class Hw7 {
     }
 
     // code for quick sort
+    // Citation
     public static <K> void quickSortInPlace(K[] S, Comparator<K> comp, int a, int b) {
         if (a > -b)
             return;
@@ -71,7 +73,136 @@ class Hw7 {
             }
 
         }
+        temp = S[left];
+        S[left] = S[right];
+        S[b] = temp;
+
+        quickSortInPlace(S, comp, a, left - 1);
+        quickSortInPlace(S, comp, left + 1, b);
     }
+    // heap sort
+    // Citation
+
+    // create random integers
+    public static void randomArr(int[] arr, int upper) {
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            arr[i] = random.nextInt(1000000) + 1;
+        }
+    }
+
+    public static void insertionTime(int arr[]) {
+        // 3. create start time
+        long startTime = System.nanoTime();
+        // 4. Run interstion sort and calc the elapsted time.
+        insertionSort(arr);
+        // 5. create end time
+        long endTime = System.nanoTime();
+        // 6. subtract the two
+        long elapsedTime = endTime - startTime;
+        System.out.println("The elapsed time for insertion sort is" + elapsedTime);
+    }
+
+    public static void mergeTime(int arr[]) {
+        // 3. create start time
+        long startTime = System.nanoTime();
+        // 4. Run interstion sort and calc the elapsted time.
+        mergeSort(null, null);
+        ;
+        // 5. create end time
+        long endTime = System.nanoTime();
+        // 6. subtract the two
+        long elapsedTime = endTime - startTime;
+        System.out.println("The elapsed time for merge sort is" + elapsedTime);
+    }
+
+    public static void quickTime(int arr[]) {
+        // 3. create start time
+        long startTime = System.nanoTime();
+        // 4. Run interstion sort and calc the elapsted time.
+        // quickSortInPlace(null);
+        // 5. create end time
+        long endTime = System.nanoTime();
+        // 6. subtract the two
+        long elapsedTime = endTime - startTime;
+        System.out.println("The elapsed time for hash sort is" + elapsedTime);
+    }
+
     // code for heap sort found on interent
+    /*
+     * Citation:
+     * Parewa Labs Pvt.Ltd.(2023,February 20).
+     * Heap sort
+     * algorithm. Heap Sort
+     * Algorithm. Retrieved February 20,2023,
+     * from https:// www.programiz.com/dsa/heap-sort
+     */
+
+    public void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            // Heapify root element
+            heapify(arr, i, 0);
+        }
+    }
+
+    /*
+     * Citation:
+     * Parewa Labs Pvt.Ltd.(2023,February 20).
+     * Heap sort
+     * algorithm. Heap Sort
+     * Algorithm. Retrieved February 20,2023,
+     * from https:// www.programiz.com/dsa/heap-sort
+     */
+
+    void heapify(int arr[], int n, int i) {
+        // Find largest among root, left child and right child
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+        // Swap and continue heapifying if root is not largest
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] num = new int[10];
+        int n = 10000;
+        // 1. create random array that is 10
+        randomArr(num, 1000000);
+        System.out.println(Arrays.toString(num));
+        // // 2. create a copy the random array
+        // int[] copyOne = Arrays.copyOf(num, 10)
+        // // 3. run the insertion sort time
+        // insertionTime();
+        // // 4. create a copy the random array
+        // int[] copyTwo = Arrays.copyOf(num, 10)
+        // // 5. run the merge sort sort time
+        // mergeTime(null);
+        // // 6. create a copy the random array
+        // int[] copyThree = Arrays.copyOf(num, 10)
+        // // 7. runn the quick sort time
+        // quickTime(num);
+        // // 8. create a copy the random array
+        // int[] copyFour = Arrays.copyOf(num, 10)
+
+        // // 9. runn the heap sort time
+
+    }
 
 }
