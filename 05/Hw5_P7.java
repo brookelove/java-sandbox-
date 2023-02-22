5ecf90a746dbbd5a448ac4d245cee5290f662c14
brookelove <brookelove@Brookes-iMac-2.local>
2023-02-21 17:17:46 -0800

    finishing upo with merge sort

--- 05/Hw5_P7.java
+++ 05/Hw5_P7.java
@@ -2,13 +2,14 @@ import java.util.Arrays;
 import java.util.Random;
 
 class Hw7 {
-    // code for insertion sort
+    // == INSRTION SORT ============================================
     public static void insertionSort(int[] data) {
         int n = data.length;
         for (int k = 1; k < n; k++) {
             int cur = data[k];
             int j = k;
-            while (j > 0 && data[j - 1] > cur) {
+            while (j > 0 && data[j - 1] > cur) { // while j is less than 0 and the data index is one less than j which
+                                                 // is less than the current
                 data[j] = data[j - 1];
                 j--;
             }
@@ -18,96 +19,66 @@ class Hw7 {
 
     // Citation: GoodRich, M., Tamassia, R., &amp; Goldwasser, M. (2014). Data
     // Structures and Algorithims in Java (Sixth). Wiley.
-    // ==========================================================================
-    void merge(int arr[], int l, int m, int r) {
-        // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        /* Create temp arrays */
-        int L[] = new int[n1];
-        int R[] = new int[n2];
-
-        /* Copy data to temp arrays */
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
-
-        /* Merge the temp arrays */
-
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
-
-        // Initial index of merged subarray array
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
+    // ====MERGE SORT
+    // ======================================================================
+    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
             } else {
-                arr[k] = R[j];
-                j++;
+                a[k++] = r[j++];
             }
-            k++;
         }
-
-        /* Copy remaining elements of L[] if any */
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+        while (i < left) {
+            a[k++] = l[i++];
         }
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+        while (j < right) {
+            a[k++] = r[j++];
         }
     }
 
-    void sort(int arr[], int l, int r) {
-        if (l < r) {
-            // Find the middle point
-            int m = l + (r - l) / 2;
-
-            // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr, m + 1, r);
-
-            // Merge the sorted halves
-            merge(arr, l, m, r);
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) { // base case
+            return;
         }
-    }
-    // https://www.geeksforgeeks.org/merge-sort/. (2023, February 17). Merge sort
-    // algorithm. GeeksforGeeks. Retrieved February 21, 2023, from
-    // https://www.geeksforgeeks.org/merge-sort/
+        int mid = n / 2;
+        int[] left = new int[mid];
+        int[] right = new int[n - mid];
 
-    // ==========================================================================
-    // quick sort
+        for (int i = 0; i < mid; i++) { // traverse the left side of the array
+            left[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) { // traverse the right side of the array
+            right[i - mid] = a[i];
+        }
+        mergeSort(left, mid);
+        mergeSort(right, n - mid);
+
+        merge(a, left, right, mid, n - mid);
+    }
+    // Baeldung. (2022, June 7). Merge sort in Java. Baeldung. Retrieved February
+    // 21, 2023, from https://www.baeldung.com/java-merge-sort
+
+    // ====QUICK SORT
+    // ======================================================================
+    // changes the array to have a temporary to swap the two integers around
     static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
+    // checks to se if the integers are smaller or greater than eavch other and
+    // traces across the array
     static int partition(int[] arr, int low, int high) {
-
         // pivot
         int pivot = arr[high];
 
-        // Index of smaller element and
-        // indicates the right position
-        // of pivot found so far
-        int i = (low - 1);
+        int i = (low - 1); // right position of the pivot
 
         for (int j = low; j <= high - 1; j++) {
-
-            // If current element is smaller
-            // than the pivot
             if (arr[j] < pivot) {
-
-                // Increment index of
-                // smaller element
                 i++;
                 swap(arr, i, j);
             }
@@ -116,41 +87,29 @@ class Hw7 {
         return (i + 1);
     }
 
+    // main area of quiick sort that initally calls the left and right side
     static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
-
-            // pi is partitioning index, arr[p]
-            // is now at right place
-            int pi = partition(arr, low, high);
-
-            // Separately sort elements before
-            // partition and after partition
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+            int part = partition(arr, low, high);
+            quickSort(arr, low, part - 1);
+            quickSort(arr, part + 1, high);
         }
     }
 
     // GeeksforGeeks. (2023, February 20). Quicksort. GeeksforGeeks. Retrieved
     // February 21, 2023, from https://www.geeksforgeeks.org/quick-sort/
-    // ==========================================================================
-    public static void randomArr(int[] arr, int upper) {
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            arr[i] = random.nextInt(1000000) + 1;
-        }
-    }
-
-    // ==========================================================================
+    // === HEAP SORT
+    // =======================================================================
     static void heapify(int arr[], int n, int i) {
         // Find largest among root, left child and right child
         int largest = i;
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-        if (l < n && arr[l] > arr[largest])
-            largest = l;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
 
-        if (r < n && arr[r] > arr[largest])
-            largest = r;
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
         if (largest != i) {
             int swap = arr[i];
             arr[i] = arr[largest];
@@ -204,7 +163,7 @@ class Hw7 {
 
     public static <K> void mergeTime(int arr[]) {
         long startTime = System.nanoTime();
-        // mergeSort();
+        mergeSort(arr, arr.length);
         long endTime = System.nanoTime();
         long elapsedTime = endTime - startTime;
         System.out.println("The elapsed time for merge sort is " + elapsedTime);
@@ -233,9 +192,17 @@ class Hw7 {
         System.out.println("The elapsed time for heap sort is " + elapsedTime);
     }
 
+    public static void randomArr(int[] arr, int upper) {
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            arr[i] = random.nextInt(100000) + 1;
+        }
+    }
+
     public static void main(String[] args) {
         int[] num = new int[10];
-        int n = 100000;
+        int n = 10000;
+        System.out.println("The current Algorithm n is: " + n);
         // 1. create random array that is 10
         randomArr(num, n);
         // 2. create a copy the random array
