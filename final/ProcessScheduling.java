335ad093fe3985944150945a6074bb7571fe37ca
brookelove <brookelove@Brookes-iMac-2.local>
2023-02-25 17:29:31 -0800

    creating priority queues

--- final/ProcessScheduling.java
+++ final/ProcessScheduling.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ProcessScheduling {
-    static PQ[] pqs = new PQ[10];
+    static PQ[] data = new PQ[10]; // created a priority quueue to hold all of the elemets at size 10
     // each iteration fo the while loop represents what occurs one time unit
     // must inrement teh current time in each iteration
     // while Q.length && D.length are !== 0// whle loop runs until every time until
@@ -15,8 +15,85 @@ public class ProcessScheduling {
     // execute the top prcess in Q for one time step
     // update the wait times of all prcocess in Q
 
+    // public static void trackTime() {
+    // System.out.println("Arrival = " + info.getId());
+    // System.out.println("Duration = " + info.getDuration());
+    // System.out.println("Run time left = " + info.getDuration());
+    // System.out.println("at time " + info.getArrivalTime());
+    // }
+    public static void executeProcess(PQ currP) {
+        int runT = currP.getDuration();
+        int atTime = currP.getArrivalTime();
+        System.out.println("Run time left = " + runT);
+        System.out.println(" at time " + atTime);
+        while (runT != 0) {
+            System.out.println("Execute process ID:" + currP.getId() + ", at time " + atTime + " Remaining: " + runT);
+            runT--;
+            atTime++;
+        }
+        // System.out.println(currP);
+        // need to check if the next priority queue is equal to the next one
+    }
+
+    public static void getLowest() {
+        int min = data[0].getPriority();
+        ;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].getPriority() < min) {
+
+            }
+        }
+    }
+
+    public static void runProcess() {
+        int i = 0;
+        int runT = data[i].getDuration();
+        // int atTime = data[i].getArrivalTime();
+
+        while (data.length != i) {
+            if (runT == 0) {
+                System.out.println("Finished running Process id = " + data[i].getId());
+            } else {
+                System.out.println("Now running Process id = " + data[i].getId());
+                System.out.println("Arrival = " + data[i].getArrivalTime());
+                System.out.println("Durraion = " + data[i].getDuration());
+                // System.out.println("Arrival = " + runT);
+                // System.out.println("Arrival = " + atTime);
+                executeProcess(data[i]);
+                i++;
+            }
+        }
+    }
+    // public static void runningProcess() {
+    // int i = 0;
+    // int maxT = 30;
+    // int runTimeL = data[i].getDuration();
+    // int rutArrive = data[i].getArrivalTime();
+    // System.out.println("\nMaximum wait time = " + maxT
+    // +"n");while(data.length!=i)
+
+    // {
+    // getLowest();
+    // System.out.println("Now running Process id = " + data[i].getId());
+    // // if (data[i].getArrivalTime() < i) {
+    // System.out.println("Arrival = " + data[i].getId());
+    // System.out.println("Duration = " + data[i].getDuration());
+    // System.out.println("Run time left = " + data[i].getDuration());
+    // System.out.println("at time " + data[i].getArrivalTime());
+
+    // // }
+    // for(int j = 0; j < data[i].getDuration(); j++) {
+    // System.out.println("Executed process ID:"+ data[i].getId() + ", at time " +
+    // "")
+    // }
+    // System.out.println(data[i]);
+    // i++;
+    // }
+
+    // }
+
     public static void main(String[] args) {
-        try {
+        try { // grab the list out of the calll
             Scanner fileInput = new Scanner(new File("process_scheduling_input.txt"));
             int index = 0;
             while (fileInput.hasNext()) {
@@ -27,18 +104,18 @@ public class ProcessScheduling {
                 int duration = Integer.parseInt(splitLine[2]);
                 int arrivalT = Integer.parseInt(splitLine[3]);
                 PQ new_item = new PQ(id, priority, duration, arrivalT);
-                pqs[index] = new_item;
+                data[index] = new_item;
                 index++;
             }
             fileInput.close();
         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         System.out.print("All the Proceses In the Priority Queue\n");
-        for (int i = 0; i < pqs.length; i++) {
-            System.out.println(pqs[i]);
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
         }
+        runProcess();
 
     }
 }
